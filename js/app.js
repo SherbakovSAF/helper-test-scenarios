6167b15727b453fa094fb338a6b3d786687c43cc
SherbakovSAF <sherbakovsaf@gmail.com>
2022-10-26 21:19:23 +0300

    v 0.0.2
    попытка что то сделать - неудачная

--- js/app.js
+++ js/app.js
@@ -1,47 +1,75 @@
-let state = [
-     {
-          id: 0,
-          question: "находясь во фракция 'Полиция' будет бить дубинкой всех подряд и садить их?",
-          command: "/jail",
-          minPunish: "0",
-          maxPunish: "150",     
-     },
-     {
-          id: 1,
-          question: "убьёт человека без причины в казино, где будет, кроме Вас 4 человека?",
-          command: "/jail",
-          minPunish: "0",
-          maxPunish: "100",
-     },
-     {
-          id: 2,
-          question: "если игрок, будет ехать по полю на охоте, на авто 'Niva'",
-          command: "-",
-     },
-     {
-          id: 3,
-          question: "если игрок, скажет в чат 'Пошли в Скайп'",
-          command: "-",
-     }
-]
+let state = {
+    questions: [
+        {
+            id: 0,
+            question:
+                "находясь во фракция 'Полиция' будет бить дубинкой всех подряд и садить их?",
+            command: "/jail",
+            minPunish: "0",
+            maxPunish: "150",
+        },
+        {
+            id: 1,
+            question:
+                "убьёт человека без причины в казино, где будет, кроме Вас 4 человека?",
+            command: "/jail",
+            minPunish: "0",
+            maxPunish: "100",
+        },
+        {
+            id: 2,
+            question:
+                "если игрок, будет ехать по полю на охоте, на авто 'Niva'",
+            command: "/jail",
+            minPunish: "0",
+            maxPunish: "100",
+        },
+        {
+            id: 3,
+            question: "если игрок, скажет в чат 'Пошли в Скайп'",
+            command: "/jail",
+            minPunish: "0",
+            maxPunish: "100",
+        },
+    ],
+    currentAnswer: {},
+};
 
-const answerInput = document.querySelector('#answer__input')
-const questionWindow = document.querySelector('#question__window')
-const initialWindow = document.querySelector('.initial__window')
-const mainWrap = document.querySelector('.main__wrap ')
+const answerInput = document.querySelector("#answer__input");
+const questionWindow = document.querySelector("#question__window");
+const initialWindow = document.querySelector(".initial__window");
+const mainWrap = document.querySelector(".main__wrap ");
 
-function resetQuestion(elem){
-     initialWindow.classList.add('none')
-     let objectQuestion = elem[Math.floor((Math.random() * elem.length))]
-     questionWindow.innerHTML = objectQuestion.question
-     mainWrap.classList.remove('none')
-     return 
+function generateQuestion(elem) {
+    let objectQuestion = elem.questions[Math.floor(Math.random() * elem.questions.length)];
+    questionWindow.innerHTML = objectQuestion;
+    elem.currentAnswer = objectQuestion;
+    console.log(elem.currentAnswer)
 }
 
-function proverka(){
-     if (answerInput.value == state[0].prover){
-          answerInput.value = 'правильно'
-     } else {
-          answerInput.value = 'НЕправильно'
-     }
+function resetQuestion(elem) {
+    initialWindow.classList.add("none");
+    generateQuestion(elem);
+    mainWrap.classList.remove("none");
+}
+
+function checkAnswer(elem) {
+    let clearArray = answerInput.value.split(" ").filter((e) => e != "");
+    numberAnswer = elem.currentAnswer
+console.log(clearArray[0], numberAnswer.command)
+    if (clearArray.length === 3) {
+        if (
+            clearArray[0] === numberAnswer.command &&
+            clearArray[1] === "number" &&
+            clearArray[2] >= numberAnswer.minPunish &&
+            clearArray[2] <= numberAnswer.maxPunish
+        ) {
+           return  console.log("ответ верный");
+        } else {
+          return console.log("ответ верный");
+        }
+    } else {
+     return console.log("Проверьте правильность написания, оно должны быть из 3 элементов. Пример: /ban 0 00");
+    }
+    
 }
