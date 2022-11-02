41a02b6a5c8699e0c96e1265765aeec123466d9a
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-02 17:54:32 +0300

    v 0.1.3
    Переделал проверку
    Добавил малые изменения в вёрстку.
    Интерфейс стал более понятный

--- Files/js/app.js
+++ Files/js/app.js
@@ -3,93 +3,151 @@ let state = {
         {
             id: 0,
             question:
-                "находясь во фракция 'Полиция' будет бить дубинкой всех подряд и садить их?",
-            command: "/kick",
-            // minPunish: 0,
-            // maxPunish: 100,
+                "Что если начал стрелять на ЖД Арзамас в человека с Desert Eagle?",
+            command: "/warn",
         },
         {
             id: 1,
             question:
-                "убьёт человека без причины в казино, где будет, кроме Вас 4 человека?",
+                "Человек стоит на ЦР, с занятой лавкой в АФК уже 5+ минут и у него нет товаров",
             command: "/kick",
-            minPunish: 0,
-            maxPunish: 100,
         },
         {
             id: 2,
             question:
-                "если игрок, будет ехать по полю на охоте, на авто 'Niva'",
-            command: "/kick",
-            minPunish: 0,
-            maxPunish: 100,
+                "А если игрок будет ехать по полю, на авто марки Niva'",
+            command: "/skip",
         },
         {
             id: 3,
-            question: "если игрок, скажет в чат 'Пошли в Скайп'",
-            command: "/kick",
+            question: "Допустим Вы видите диалог, между двумя игроками. И один другому говорит, пошли в 'Скайп'. Что Вы сделаете?",
+            command: "/mute",
             minPunish: 0,
-            maxPunish: 100,
+            maxPunish: 70,
         },
     ],
     currentAnswer: {},
 };
 
-const answerInput = document.querySelector("#answer__input");
-const questionWindow = document.querySelector("#question__window");
-const initialWindow = document.querySelector(".initial__window");
-const mainWrap = document.querySelector(".main__wrap");
-const answer = document.querySelector(".answer");
 
 
+const answerInput = document.querySelector("#answerInput")
+
+// Кнопка создать пост
 function generateQuestion(elem) {
+    const createQuestion = document.querySelector("#createQuestion")
+    const questionWindow = document.querySelector("#questionWindow")
+    const answerButton = document.querySelector("#answerButton")
     let objectQuestion =
         elem.questions[Math.floor(Math.random() * elem.questions.length)];
     questionWindow.innerHTML = objectQuestion.question;
     elem.currentAnswer = objectQuestion;
+    createQuestion.classList.add("none")
+    answerInput.removeAttribute("disabled")
+    answerButton.classList.remove("none")
+    answerInput.value = ""
     console.log(elem.currentAnswer);
 }
 
-function resetQuestion(elem) {
-    initialWindow.classList.add("none");
-    generateQuestion(elem);
-    mainWrap.classList.remove("none");
+function result(solution){
+    answerInput.className = ""
+    answerInput.className = `answer__input  ${solution}`
 }
-
-function windowTotal(text, className) {
-    answer.firstElementChild.innerHTML = text
-    answer.firstElementChild.classList.add(className)
-    answer.classList.remove("none");
-    answerInput.value = ""
-}
-
 
 function checkAnswer(elem) {
+    const trueAnswerCount = document.querySelector("#trueAnswerCount")
+    const falseAnswerCount = document.querySelector("#falseAnswerCount")
     let clearArray = answerInput.value.split(" ").filter((e) => e != "");
     numberAnswer = elem.currentAnswer;
-    if (clearArray.length === 3) {
-        if (
-            clearArray[0] === numberAnswer.command &&
-            typeof Number(clearArray[1]) == "number" &&
-            clearArray[2] >= numberAnswer.minPunish &&
-            clearArray[2] <= numberAnswer.maxPunish
-        ) {
-            return console.log("ответ верный");
-        } else {
-            windowTotal("Ответ неверный", "bad");
-        }
-    } else if (clearArray.length === 2) {
-        if (
-            clearArray[0] === numberAnswer.command &&
-            typeof Number(clearArray[1]) == "number"
-        ) {
-            return console.log("ответ верный");
-        } else {
-            windowTotal("Ответ неверный", "bad");
-        }
-    } else {
-        return console.log(
-            "Проверьте правильность написания, оно должны быть из 3 элементов. Пример: /ban 0 00"
-        );
+    console.log(clearArray);
+    switch (clearArray[0]) {
+        case "/mute":
+        case "/ban":
+        case "/jail":
+            if (
+                typeof Number(clearArray[1]) == "number" &&
+                clearArray[2] >= numberAnswer.minPunish &&
+                clearArray[2] <= numberAnswer.maxPunish &&
+                clearArray.length === 3
+            ) {
+                result("answer__true");
+                trueAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+            } else {
+                result("answer__false");
+                falseAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+            }
+            break;
+        case "/kick":
+        case "/warn":
+            if (typeof Number(clearArray[1]) == "number" &&
+            clearArray.length === 2) {
+                result("answer__true");
+                trueAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+            } else {
+                result("answer__false");
+                falseAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+            }
+            break;
+        case "/skip":
+            if (clearArray.length === 1) {
+                result("answer__true");
+                trueAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+            } else {
+                result("answer__false");
+                falseAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+            }
+            break;
+        default:
+            result("answer__warning");
     }
+
+    // if (clearArray.length === 3) {
+    //     if (
+    //         clearArray[0] === numberAnswer.command &&
+    //         typeof Number(clearArray[1]) == "number" &&
+    //         clearArray[2] >= numberAnswer.minPunish &&
+    //         clearArray[2] <= numberAnswer.maxPunish
+    //     ) {
+    //         result("answer__true")
+    //     } else {
+    //         result("answer__false")
+    //     }
+    // } else if (clearArray.length === 2) {
+    //     if (
+    //         clearArray[0] === numberAnswer.command &&
+    //         typeof Number(clearArray[1]) == "number"
+    //     ) {
+    //         result("answer__true")
+    //     } else {
+    //         result("answer__false")
+    //     }
+    // } else if (clearArray.length === 1) {
+    //     if (clearArray[0] === numberAnswer.command) {
+    //         result("answer__true")
+    //     } else {
+    //         result("answer__false")
+    //     }
+    // } else {
+    //     result("answer__warning")
+    // }
 }
+
+
+// function resetQuestion(elem) {
+//     initialWindow.classList.add("none");
+//     generateQuestion(elem);
+//     mainWrap.classList.remove("none");
+// }
+
+
+
+// function windowTotal(text, className) {
+//     answer.firstElementChild.innerHTML = text
+//     answer.firstElementChild.classList.add(className)
+//     answer.classList.remove("none");
+//     answerInput.value = ""
+// }
+
+
+
+
