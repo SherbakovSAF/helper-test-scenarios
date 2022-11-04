55f5a2decb97253118d1cc7f20897b3f65b68966
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-04 23:32:36 +0300

    v 0.1.5
    Почти всё. Осталось полнить всё вопросами. Работает всё идеально
    - Сделал возможность писать в любом регистре
    - Сделал функцию, чтобы вопросы не повторялись и администратор мог прийти в тупик

--- Files/js/app.js
+++ Files/js/app.js
@@ -14,13 +14,13 @@ let state = {
         },
         {
             id: 2,
-            question:
-                "А если игрок будет ехать по полю, на авто марки Niva'",
+            question: "А если игрок будет ехать по полю, на авто марки Niva'",
             command: "/skip",
         },
         {
             id: 3,
-            question: "Допустим Вы видите диалог, между двумя игроками. И один другому говорит, пошли в 'Скайп'. Что Вы сделаете?",
+            question:
+                "Допустим Вы видите диалог, между двумя игроками. И один другому говорит, пошли в 'Скайп'. Что Вы сделаете?",
             command: "/mute",
             minPunish: 0,
             maxPunish: 70,
@@ -29,49 +29,81 @@ let state = {
     currentAnswer: {},
 };
 
-
-
-const answerInput = document.querySelector("#answerInput")
-const createQuestion = document.querySelector("#createQuestion")
-    const questionWindow = document.querySelector("#questionWindow")
-    const answerButton = document.querySelector("#answerButton")
+const answerInput = document.querySelector("#answerInput");
+const createQuestion = document.querySelector("#createQuestion");
+const questionWindow = document.querySelector("#questionWindow");
+const answerButton = document.querySelector("#answerButton");
+let questionArray = [];
 
 // Кнопка создать пост
 function generateQuestion(elem) {
-    let objectQuestion = elem.questions[Math.floor(Math.random() * elem.questions.length)];
-    questionWindow.innerHTML = objectQuestion.question;
+    
+    if (questionArray.length >= elem.questions.length) {
+
+        questionWindow.innerHTML =
+            "Офигеть. Ты ответил на все вопросы. Перезагрузи страницу и всё начнётся по-новой, Миша";
+            questionWindow.style.color = "#a8323e"
+            answerButton.style.opacity = 1;
+    answerButton.setAttribute("disabled", "");
+    answerButton.style.opacity = 0;
+    createQuestion.style.opacity = 0;
+    createQuestion.setAttribute("disabled", "");
+    answerInput.style.opacity = 0;
+    answerInput.setAttribute("disabled", "");
+        return;
+    }
+    debugger
+    let objectQuestion =
+        elem.questions[Math.floor(Math.random() * elem.questions.length)];
+    if (questionArray.indexOf(objectQuestion.id) < 0) {
+        questionArray.push(objectQuestion.id);
+        questionWindow.innerHTML = objectQuestion.question;
     elem.currentAnswer = objectQuestion;
-    answerInput.removeAttribute("disabled")
-    answerInput.value = ""
-    createQuestion.style.opacity = 0
-    answerButton.style.opacity = 1
-    createQuestion.setAttribute("disabled","")
-    answerButton.removeAttribute("disabled")
+    answerInput.removeAttribute("disabled");
+    answerInput.value = "";
+    answerInput.className = "answer__input";
+    createQuestion.style.opacity = 0;
+    createQuestion.setAttribute("disabled", "");
+    answerButton.style.opacity = 1;
+    answerButton.removeAttribute("disabled");
+    } else {
+        generateQuestion(elem)
+        return
+    }
+}
+
+function change(elem) {
+    
 }
 
 function nextQuestion(atr) {
     if (atr == true) {
-        questionWindow.innerHTML = "Ты большой молодец. Создай ещё один вопрос" 
-    } else if (atr == false){
-        questionWindow.innerHTML = "Посмотри, где ты совершил ошибку" + "<br/>" + questionWindow.innerHTML
+        questionWindow.innerHTML = "Ты большой молодец. Создай ещё один вопрос";
+    } else if (atr == false) {
+        questionWindow.innerHTML =
+            "Посмотри, где ты совершил ошибку" +
+            "<br/>" +
+            questionWindow.innerHTML;
     }
-    answerInput.setAttribute("disabled", '')
-    answerInput.value = "Чтобы создать новый вопрос, нажмите кнопку 'Создать вопрос'"
-    createQuestion.style.opacity = 1
-    answerButton.style.opacity = 0
-    answerButton.setAttribute("disabled","")
-    createQuestion.removeAttribute("disabled")
+    answerInput.setAttribute("disabled", "");
+    answerInput.value =
+        "Чтобы создать новый вопрос, нажмите кнопку 'Создать вопрос'";
+    createQuestion.style.opacity = 1;
+    answerButton.style.opacity = 0;
+    answerButton.setAttribute("disabled", "");
+    createQuestion.removeAttribute("disabled");
 }
 
-function result(solution){
-    answerInput.className = ""
-    answerInput.className = `answer__input  ${solution}`
+function result(solution) {
+    answerInput.className = "";
+    answerInput.className = `answer__input  ${solution}`;
 }
 
 function checkAnswer(elem) {
-    const trueAnswerCount = document.querySelector("#trueAnswerCount")
-    const falseAnswerCount = document.querySelector("#falseAnswerCount")
+    const trueAnswerCount = document.querySelector("#trueAnswerCount");
+    const falseAnswerCount = document.querySelector("#falseAnswerCount");
     let clearArray = answerInput.value.split(" ").filter((e) => e != "");
+    clearArray[0] = clearArray[0].toLowerCase();
     numberAnswer = elem.currentAnswer;
     console.log(clearArray);
     switch (clearArray[0]) {
@@ -85,11 +117,15 @@ function checkAnswer(elem) {
                 clearArray[2] <= numberAnswer.maxPunish &&
                 clearArray.length === 3
             ) {
-                nextQuestion(true)
-                trueAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+                result("answer__true");
+                nextQuestion(true);
+                trueAnswerCount.innerHTML =
+                    Number(trueAnswerCount.innerHTML) + 1;
             } else {
-                nextQuestion(false)
-                falseAnswerCount.innerHTML = Number(falseAnswerCount.innerHTML) + 1
+                result("answer__false");
+                nextQuestion(false);
+                falseAnswerCount.innerHTML =
+                    Number(falseAnswerCount.innerHTML) + 1;
             }
             break;
         case "/kick":
@@ -97,76 +133,36 @@ function checkAnswer(elem) {
             if (
                 clearArray[0] == numberAnswer.command &&
                 typeof Number(clearArray[1]) == "number" &&
-            clearArray.length === 2) {
-                nextQuestion(true)
-                trueAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+                clearArray.length === 2
+            ) {
+                result("answer__true");
+                nextQuestion(true);
+                trueAnswerCount.innerHTML =
+                    Number(trueAnswerCount.innerHTML) + 1;
             } else {
-                nextQuestion(false)
-                falseAnswerCount.innerHTML = Number(falseAnswerCount.innerHTML) + 1
+                result("answer__false");
+                nextQuestion(false);
+                falseAnswerCount.innerHTML =
+                    Number(falseAnswerCount.innerHTML) + 1;
             }
             break;
         case "/skip":
             if (
                 clearArray[0] == numberAnswer.command &&
-                clearArray.length === 1) {
-                nextQuestion(true)
-                trueAnswerCount.innerHTML = Number(trueAnswerCount.innerHTML) + 1
+                clearArray.length === 1
+            ) {
+                result("answer__true");
+                nextQuestion(true);
+                trueAnswerCount.innerHTML =
+                    Number(trueAnswerCount.innerHTML) + 1;
             } else {
-                nextQuestion(false)
-                falseAnswerCount.innerHTML = Number(falseAnswerCount.innerHTML) + 1
+                result("answer__false");
+                nextQuestion(false);
+                falseAnswerCount.innerHTML =
+                    Number(falseAnswerCount.innerHTML) + 1;
             }
             break;
         default:
             result("answer__warning");
     }
-
-    // if (clearArray.length === 3) {
-    //     if (
-    //         clearArray[0] === numberAnswer.command &&
-    //         typeof Number(clearArray[1]) == "number" &&
-    //         clearArray[2] >= numberAnswer.minPunish &&
-    //         clearArray[2] <= numberAnswer.maxPunish
-    //     ) {
-    //         result("answer__true")
-    //     } else {
-    //         result("answer__false")
-    //     }
-    // } else if (clearArray.length === 2) {
-    //     if (
-    //         clearArray[0] === numberAnswer.command &&
-    //         typeof Number(clearArray[1]) == "number"
-    //     ) {
-    //         result("answer__true")
-    //     } else {
-    //         result("answer__false")
-    //     }
-    // } else if (clearArray.length === 1) {
-    //     if (clearArray[0] === numberAnswer.command) {
-    //         result("answer__true")
-    //     } else {
-    //         result("answer__false")
-    //     }
-    // } else {
-    //     result("answer__warning")
-    // }
 }
-
-
-// function resetQuestion(elem) {
-//     initialWindow.classList.add("none");
-//     generateQuestion(elem);
-//     mainWrap.classList.remove("none");
-// }
-
-
-
-// function windowTotal(text, className) {
-//     answer.firstElementChild.innerHTML = text
-//     answer.firstElementChild.classList.add(className)
-//     answer.classList.remove("none");
-//     answerInput.value = ""
-// }
-
-
-
-
